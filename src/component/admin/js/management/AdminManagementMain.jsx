0e81e10a2daa1b027be104e0703e6bbf2028d229
Choiinhwan11 <Choiinhwan11>
2025-06-17 13:18:33 +0900

    management

---
+++ src/component/admin/js/management/AdminManagementMain.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import AdminHeaderMain from "../header/AdminHeaderMain";
+
+const AdminManagementMain = () => {
+    return (
+        <div>
+
+            <AdminHeaderMain/>
+        </div>
+    );
+};
+
+export default AdminManagementMain;
